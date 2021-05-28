c298324bac37d03d6a10510b106bfc56444f12f8
michcho415 <michcho415@student.polsl.pl>
2021-05-28 20:50:57 +0200

    Seeder class for hall table in database
    Added seeder class for hall table, adding in Program.cs

---
+++ Backend/Data/HallSeeder.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Backend.Entities;
+
+namespace Backend.Data
+{
+    public class HallSeeder
+    {
+        private readonly DataContext context;
+
+        
+        public HallSeeder(DataContext context) => this.context = context;
+        
+        
+        public void SeedData()
+        {
+            if (context.Halls.Any())
+                return;
+            else
+            {
+                context.Halls.AddRange(GetHalls());
+                context.SaveChanges();
+            }
+        }
+
+        private ICollection<Hall> GetHalls()
+        {
+            var halls = new List<Hall>();
+            
+            halls.Add(new Hall
+            {
+                Seats = 100
+            });
+            
+            halls.Add(new Hall
+            {
+                Seats = 120
+            });
+            
+            halls.Add(new Hall
+            {
+                Seats = 80
+            });
+            
+            halls.Add(new Hall
+            {
+                Seats = 125
+            });
+            
+            halls.Add(new Hall
+            {
+                Seats = 130
+            });
+            
+            halls.Add( new Hall
+            {
+                Seats = 200
+            });
+            
+            halls.Add( new Hall
+            {
+                Seats = 120
+            });
+            
+            halls.Add( new Hall
+            {
+                Seats = 200
+            });
+            
+            return halls;
+        }
+        
+    }
+}
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -30,6 +30,7 @@ namespace Backend
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Backend", Version = "v1"}); });
 
             services.AddTransient<IAdminRepository,AdminRepository>();
+            services.AddTransient<HallSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
