66849c85d4db3c9211b97a194eb15c786e10d75c
Leszek <lenzuolo@wp.pl>
2021-05-30 22:10:39 +0200

    Added Halls repository
    Added Halls repository for further development

---
+++ Backend/Repositories/HallsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Backend.Data;
+using Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repositories
+{
+    public class HallsRepository : IHallsRepository
+    {
+        private readonly DataContext context;
+
+        public HallsRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        
+        public async Task<Hall> GetAsync(int id)
+        {
+            return await context.Halls.FindAsync(id);
+        }
+
+        public async Task<Hall> GetAsync(string letterCode)
+        {
+            return await context.Halls.FirstOrDefaultAsync(h => h.HallLetter == letterCode);
+        }
+    }
+}
---
+++ Backend/Repositories/IHallsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Backend.Entities;
+
+namespace Backend.Repositories
+{
+    public interface IHallsRepository
+    {
+        Task<Hall> GetAsync(int id);
+        Task<Hall> GetAsync(string letterCode);
+    }
+}
